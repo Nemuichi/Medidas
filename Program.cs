ce187b632791defaff4d5b59083bc3ce8a6d574c
Ramon Dias <ramondiasperes04@gmail.com>
2025-10-02 20:48:03 -0300

    programa feito

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
-﻿string kmMedida, metroMedida, cmMedida;
+﻿double kmMedida, metroMedida, cmMedida;
 
-
-metroMedida = Int
 
 Console.Write("Entre com a medida em metros: ");
+metroMedida = Convert.ToDouble(Console.ReadLine()!);
+
+kmMedida = metroMedida / 1000;
+cmMedida = metroMedida * 100;
 
 
+Console.WriteLine("Conversão em:");
 
-
-
-
-
-
+Console.WriteLine($"Centímetros: {cmMedida}");
+Console.WriteLine($"Metro: {metroMedida}");
+Console.WriteLine($"Quilômetro: {kmMedida}");
